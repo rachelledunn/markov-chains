0a78525bfc832d2c6273142a8f226df6f1979aea
Hackbright Student <info@hackbrightacademy.com>
2015-04-14 16:59:31 -0700

    Takes dictionary of Markov chains, and returns random text.

--- markov.py
+++ markov.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
 
@@ -26,7 +25,9 @@ def make_chains(corpus):
         else: # if this key does not exist, create key and value
             markov_dict[(corpus_list[i], corpus_list[i + 1])] = [corpus_list[i + 2]]
     
-
+    print "The Dictionary:"
+    print markov_dict
+    return markov_dict
 
     """
     Our pseudocode, first thoughts:
@@ -57,31 +58,76 @@ def make_chains(corpus):
     """
 
 
-    return markov_dict
+
+def make_text(chains):
+    """Takes dictionary of markov chains; returns random text."""
+
+    new_string = ''
+
+    first_item = chains.popitem() # pick an item from the dictionary at "random"
+
+    k, v = first_item # unpack the key and value from the item
+
+    new_string = k[0] + ' ' + k[1] # concatenating the words from the key tuple into a string
+
+    next_tuple = (k[1], v.pop()) # create the next tuple by using the second word in the tuple and one word from the value
+
+    while next_tuple in chains: # while the next tuple is included in the dictionary
+        if next_tuple in chains.keys():
+            new_value = chains[next_tuple] # sets the value of new_value if next_tuple exists in chains
+            new_value = new_value.pop() # sets new_value to random value of existing value list
+
+            next_tuple = (next_tuple[1], new_value)
+        
+        new_string = new_string + ' ' + next_tuple[1]
+            
+    return new_string
+
+    """
+    new_string = ''
+
+    first_tuple_item = k[0]
+    while an item exists in the dictionary, continue this loop:        
+        find a random first tuple
+        separate key and value
+        turn key into a string
+        append or concatenate to new_string
+        use value checking for matching key in dictionary
+
+    while chains.iteritems() in chains:
+        then take each tuple, pull each string out and add it to new_string
+        then take randomly a value
+        then check for that value against existing key[0]
+        then take that tuple, pull each string out and add it to the new_string
+
+    return new_string
+
+    Our pseudocode:
+    Using our Markov dictionary from make_chains(),
+    using .popitem(), get first key for making new string of text.
+
+    (Would, you) -> could could could like like could
+    (could, you) -> in with in with
+    (in, a) -> house, box
+    searching tuples for house... -> does not exist, end function, end of new string
+
+    new_string = Would you could you in a house
+    """
 
 
-# def make_text(chains):
-#     """Takes dictionary of markov chains; returns random text."""
+# Change this to read input_text from a file, deciding which file should
+# be used by examining the `sys.argv` arguments (if neccessary, see the
+# Python docs for sys.argv)
 
-#     return "Here's some random text."
 
+script,filename = sys.argv
 
-# # Change this to read input_text from a file, deciding which file should
-# # be used by examining the `sys.argv` arguments (if neccessary, see the
-# # Python docs for sys.argv)
+input_text = filename
 
-# # import fileinput
-# # for line in fileinput.input():
-# #     process(line)
+# Get a Markov chain
+chain_dict = make_chains(input_text)
 
-input_text = "green-eggs.txt"
+# Produce random text
+random_text = make_text(chain_dict)
 
-# # Get a Markov chain
-# chain_dict = make_chains(input_text)
-
-print make_chains(input_text)
-
-# # Produce random text
-# random_text = make_text(chain_dict)
-
-# print random_text
+print random_text
